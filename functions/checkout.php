c4e42b398824185f7dc85c346b7004a90bb5ec42
zdungpham <dungphamvan973@gmail.com>
2024-05-07 21:27:36 +0700

    2

--- functions/checkout.php
+++ functions/checkout.php
@@ -1,6 +1,6 @@
 <?php
 require_once("database.php");
-function order_create($order_info, $products, $cart){
+function order_create($order_info,$products,$cart)  {
     $customer_name = $order_info["customer_name"];
     $tel = $order_info["tel"];
     $address = $order_info["address"];
@@ -8,14 +8,13 @@ function order_create($order_info, $products, $cart){
     $status = $order_info["status"];
     $shipping_method = $order_info["shipping_method"];
     $payment_method = $order_info["payment_method"];
-
-    $sql = "INSERT INTO orders(customer_name, tel, address, grand_total, status, shipping_method, payment_method)
-        VALUES($customer_name, $tel, $address, $grand_total, $status, $shipping_method, $payment_method); ";
-
-    $order_id = insert_get_id($sql);
+    $sql = "insert into orders(customer_name,tel,address,grand_total,status,shipping_method,payment_method) 
+        values('$customer_name','$tel','$address',$grand_total,
+        '$status','$shipping_method','$payment_method')";
+    $order_id = insert_get_id($sql);    
     foreach($products as $item){
-        //insert product to order_products
-
-        //update qty products table
+        // insert product to order_products 
+        
+        // update qty products table
     }
 }
--- place_order.php
+++ place_order.php
@@ -1,16 +1,16 @@
-<?php 
-session_start();// open session
-require_once("funtions/cart.php");
-require_once("funtions/checkout.php");
+<?php
+session_start();
+require_once("functions/cart.php");
+require_once("functions/checkout.php");
 
-$customer_name = $_POST["customer_name"];
+$customer_name = $_POST['customer_name'];
 $tel = $_POST["tel"];
 $address = $_POST["address"];
 $shipping_method = $_POST["shipping_method"];
 $payment_method = $_POST["payment_method"];
 
 $cart = $_SESSION["cart"];
-if(count($cart) ==0){
+if(count($cart)== 0){
     header("Location: cart.php");
 }
 $products = get_cart();
@@ -19,21 +19,18 @@ foreach($products as $item){
     $grand_total += $item["price"] * $cart[$item["id"]];
 }
 switch($shipping_method){
-    case "FREE": BREAK;
-    case "STANDARD": $grand_total += 10;BREAK;
-    case "EXPRESS": $grand_total += 20;BREAK;
-    
+    case "FREE": break;
+    case "STANDARD": $grand_total+=10;break;
+    case "EXPRESS": $grand_total+=20;break;
 }
-
 $status = "PENDING";
 $order_info = [
-    "customer_name" => $customer_name,
-    "tel" => $tel,
-    "address" => $address,
-    "grand_total" => $grand_total,
-    "status" => $status,
-    "shipping_method" => $shipping_method,
-    "payment_method" => $payment_method,
+    "customer_name" =>$customer_name,
+    "tel" =>$tel,
+    "address" =>$address,
+    "grand_total" =>$grand_total,
+    "status" =>$status,
+    "shipping_method" =>$shipping_method,
+    "payment_method" =>$payment_method,
 ];
-
-order_create($order_info, $products, $cart);
+order_create($order_info,$products,$cart);
